8cd624fcdf84c46d38fc346bf99931075290889a
Dewald de Bruyn <dewalddebruyn@gmail.com>
2018-03-31 17:12:56 +0200

    Testing text parsing

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 from flask import Flask
+
+from describe_image import analyze
+from parser import getTheories
+from matcher import match
+
+app = Flask(__name__)
+text = getTheories('theories.txt')
+
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-  return 'Hello, World!'
+  return str(text)
 
 if __name__ == '__main__':
   app.run()

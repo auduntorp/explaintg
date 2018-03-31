9f030d47116a3a593f1c2d8b4404949181c84209
Dewald de Bruyn <dewalddebruyn@gmail.com>
2018-03-31 17:10:44 +0200

    Testing hello world

--- main.py
+++ main.py
@@ -1,37 +1,9 @@
-#from flask import Flask, request, render_template
+from flask import Flask
+app = Flask(__name__)
 
-#from describe_image import analyze
-#rom parser import getTheories
-#from matcher import match
-
-#app = Flask(__name__)
-#text = getTheories('theories.txt')
-
-@app.route('/', methods=['GET'])
+@app.route('/')
 def hello_world():
-    print("get")
-    #return open('upload_photo.html', 'r').read()
-    return render_template('index.html')
-
-
-@app.route('/', methods=['POST'])
-def analyze_image():
-    print("post woo12")
-    #filename = request.form['file']
-    #print(filename)
-    print(request.files)
-    file = request.files['file']
-    print(file)
-    print('analyzing')
-    keywords = analyze(file)
-    print('matching')
-    m = match(keywords, text)
-    if m:
-        key, title, description = m
-        return render_template('explain.html', title=title)
-    else:
-        'No match found'
+  return 'Hello, World!'
 
 if __name__ == '__main__':
-    return 'Hello, Azure'
-    #app.run()
+  app.run()

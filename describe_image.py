d81f6fa557515b213af4633ce7e1becd9ebf6774
Audun Torp <audun@telenordigital.com>
2018-03-31 13:00:46 +0200

    Accept posted images

--- describe_image.py
+++ describe_image.py
@@ -13,7 +13,6 @@ def analyze(image):
                'Ocp-Apim-Subscription-Key': 'f40b6c513c824008b8f9a2a18c85f772'}
     analyze_url = 'https://westcentralus.api.cognitive.microsoft.com/vision/v1.0/analyze?visualFeatures=description'
     response = requests.post(analyze_url, data=image, headers=headers)
-    print(response)
     return response.json().get('description').get('tags')
 
 if __name__ == '__main__':

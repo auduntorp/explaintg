56dfdad0c1a65859dc44de895f408d85494e648e
Dewald de Bruyn <dewalddebruyn@gmail.com>
2018-03-31 14:03:12 +0200

    Added matching script and test image

---
+++ matcher.py
@@ -0,0 +1,25 @@
+from describe_image import *
+from parser import *
+
+def match(keywords, theories):
+
+	for key in keywords:
+		for title in theories.keys():
+			if key in title:
+				description = theories[title]
+				return key, title, description
+
+	for key in keywords:
+		for description in theories.values():
+			if key in description:
+				for title in theories.keys():
+					if theories[title] == description:
+						return key, title, description
+
+	return None
+
+if __name__ == '__main__':
+	keywords = describe('IMG_0019.jpg')
+	print(keywords)
+	text = getTheories('theories.txt')
+	print(match(keywords, text))

4d89611c785e1473cfe6b43a6566d0bdc8398406
Audun Torp <audun@telenordigital.com>
2018-03-31 13:33:39 +0200

    Add post support

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask
+from flask import Flask, request
+
+from describe_image import analyze
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+
+@app.route('/', methods=['POST'])
+def hello_world():
+    return str(analyze(request.data))
 
 if __name__ == '__main__':
     app.run()

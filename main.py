c80df5d9cdcdc6764d58fa4cefc43ba55f8ebba0
Dewald de Bruyn <dewalddebruyn@gmail.com>
2018-03-31 17:20:36 +0200

    Moved getTheories into main

--- main.py
+++ main.py
@@ -1,8 +1,33 @@
 from flask import Flask
 
-#from describe_image import analyze
-from parser import getTheories
-#from matcher import match
+def getTheories(text):
+
+    theories = dict()
+
+    with open(text) as f:
+        data = f.readlines()
+
+    for line in data:
+        if line.strip() == '':
+            continue
+
+        try:
+            number = int(line)
+            #print(str(number) + ' ', end='')
+            mode = 'key'
+        except ValueError:
+            if mode == 'key':
+                key = line.strip()
+                #print(key)
+                theories[key] = ''
+                mode = 'value'
+                cnt  = 0
+            else:
+                if cnt > 0:
+                    theories[key] += '\n\n'
+                theories[key] += line.strip()
+                cnt = cnt + 1
+    return theories
 
 app = Flask(__name__)
 text = getTheories('theories.txt')
